1ea8c240812b4be3e6d041c1c228090c49783b86
Ryan T Robinson <robinsonr355@gmail.com>
2025-03-11 19:11:45 -0400

    Update Constants.java
    added new configs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,18 +1,100 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 
 public class Constants {
-  public static class Robot {
-    public static final double k_width = 26; // Inches
-    public static final double k_length = 28; // Inches
+  public static final class DriveConstants {
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+
+    // Chassis configuration
+    public static final double kTrackWidth = Units.inchesToMeters(23.5);
+    // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = Units.inchesToMeters(28.5);
+    // Distance between front and back wheels on robot
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // Angular offsets of the modules relative to the chassis in radians
+    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+    public static final double kFrontRightChassisAngularOffset = 0;
+    public static final double kBackLeftChassisAngularOffset = Math.PI;
+    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+
+    // SPARK MAX CAN IDs
+    public static final int kFrontLeftDrivingCanId = 11;
+    public static final int kRearLeftDrivingCanId = 13;
+    public static final int kFrontRightDrivingCanId = 15;
+    public static final int kRearRightDrivingCanId = 17;
+
+    public static final int kFrontLeftTurningCanId = 10;
+    public static final int kRearLeftTurningCanId = 12;
+    public static final int kFrontRightTurningCanId = 14;
+    public static final int kRearRightTurningCanId = 16;
+
+    public static final boolean kGyroReversed = false;
   }
+
+  public static final class ModuleConstants {
+    // The MAXSwerve module can be configured with one of three pinion gears: 12T,
+    // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+    // more teeth will result in a robot that drives faster).
+    public static final int kDrivingMotorPinionTeeth = 14;
+
+    // Calculations required for driving motor conversion factors and feed forward
+    public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
+    public static final double kWheelDiameterMeters = 0.0762;
+    public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+    // teeth on the bevel pinion
+    public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
+    public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+        / kDrivingMotorReduction;
+  }
+  
+  public static final class OIConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final double kDriveDeadband = 0.05;
+    public static final int kOperatorControllerPort = 1;
+  }
+
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+    public static final double kPXController = 1;
+    public static final double kPYController = 1;
+    public static final double kPThetaController = 1;
+
+    // Constraint for the motion profiled robot angle controller
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  }
+
+    public static final class NeoMotorConstants {
+    public static final double kFreeSpeedRpm = 5676;
+  }
+
+  //public static final class Climber{
+    //public static final int kClimberMotorId = 30;
+  //}
 
   public static class Elevator {
     public static final int kElevatorLeftMotorId = 9;
-    public static final int kElevatorRightMotorId = 10;
+    public static final int kElevatorRightMotorId = 8;
 
     public static final double kP = 0.15;
     public static final double kI = 0;
@@ -23,51 +105,56 @@ public class Constants {
     public static final double kMaxVelocity = 65;
     public static final double kMaxAcceleration = 200;
 
-    public static final int kMaxCurrent = 40;
-    public static final double kMaxPowerUp = -0.1;
-    public static final double kMaxPowerDown = 0.1;
+    public static final int kMaxCurrent = 60;
 
     public static final double kStowHeight = 0.0;
-    public static final double kL2Height = 9.0;
-    public static final double kL3Height = 25.14;
-    public static final double kL4Height = 52.0;
-    public static final double kMaxHeight = 56.2;
+    public static final double kL2Height = 9.5;
+    public static final double kL3Height = 25.75;
+    public static final double kL4Height = 54.0;
+    public static final double kMaxHeight = 54.0;
     public static final double kGroundAlgaeHeight = 0.0;
     public static final double kScoreAlgaeHeight = 0.0;
     public static final double kLowAlgaeHeight = 24.8;
     public static final double kHighAlgaeHeight = 42.5;
+
+    public static final double kSetpointThreshold = 2.0; // Acceptable distance in encoder units between the elevator's actual position and its current target height.
+
+    // Auto constants for Elevator
+    public static final double kElevatorMaxMoveTime = 2.0; // Time it takes for the elevator to move from StowHeight to MaxHeight
   }
 
   public static class Coral {
-    public static final int kLeftMotorId = 11;
-    public static final int kRightMotorId = 12;
+    public static final int kLeftMotorId = 21;//updated to match our architecture
+    public static final int kRightMotorId = 23;//updated to match our architecture
 
-    public static final int kLaserId = 0;
-    public static final int kColorId = 16;
+    public static final int kLaserId = 33;  //updated to match our architecture
+    //public static final int kColorId = 32;  //updated to match our architecture but this isn't used
 
-    public static final double kMaxCurrent = 20;
+    public static final double kMaxCurrent = 30;
 
     public static final double kP = 0.0;
     public static final double kI = 0.0;
     public static final double kD = 0.0;
     public static final double kIZone = 0;
 
-    public static final double kIntakeSpeed = 0.3;
-    public static final double kReverseSpeed = -0.3;
-    public static final double kL1Speed = 0.4;
-    public static final double kL24Speed = 0.4;
-    public static final double kIndexSpeed = 0.1;
+    public static final double kIntakeSpeed = 0.75;
+    public static final double kReverseSpeed = -0.75;
+    public static final double kL1Speed = 0.40;
+    public static final double kL24Speed = 0.10;
+    public static final double kIndexSpeed = 0.45;
     public static final double kSpeedDifference = kL1Speed * 0.5;
+
+    // Auto constants for Coral
+    public static final double kCoralScoreTime = 2.0; // Time it takes for the coral to be scored.
   }
 
   public static class Algae {
     // WRIST
-    public static final int kWristMotorId = 13;
-    public static final int kIntakeMotorId = 14;
+    public static final int kWristMotorId = 22; //updated to match our architecture
+    public static final int kIntakeMotorId = 20;   //updated to match our architecture
 
-    public static final int kWristEncoderId = 9;
-
-    public static final int kMaxWristCurrent = 10;
+    public static final int kWristEncoderId = 0;    //this is a DIO input for the through bore Encoder
+    public static final int kMaxWristCurrent = 10;  
 
     public static final double kWristP = 0.01;
     public static final double kWristI = 0.0;
@@ -83,96 +170,25 @@ public class Constants {
     public static final double kWristMaxVelocity = 690.0;
     public static final double kWristMaxAcceleration = 1380.0;
 
-    public static final double kStowAngle = 233.0;
-    public static final double kDeAlgaeAngle = 215.0;
-    public static final double kGroundIntakeAngle = 162.0;
+    public static final double kStowAngle = 155.0;
+    public static final double kDeAlgaeAngle = 250.0;
+    public static final double kGroundIntakeAngle = 335.0;
 
     // INTAKE
     public static final int kMaxIntakeCurrent = 20;
 
-    public static final double kIntakeSpeed = 0.6;
-    public static final double kEjectSpeed = -0.3;
-    public static final double kGroundIntakeSpeed = -0.3;
+    public static final double kIntakeSpeed = 1.0;
+    public static final double kEjectSpeed = -0.5;
+    public static final double kGroundIntakeSpeed = -0.75;
   }
 
-  public static class Intake {
-    // Motors
-    public static final int kIntakeMotorId = 9;
-    public static final int kPivotMotorId = 10;
-
-    // DIO
-    public static final int k_pivotEncoderId = 0;
-    public static final int k_intakeLimitSwitchId = 2;
-
-    // Absolute encoder offset
-    public static final double k_pivotEncoderOffset = 0.166842; // Straight up, sketchy to reset to "up"
-
-    // Pivot set point angles
-    public static final double k_pivotAngleGround = 60;
-    public static final double k_pivotAngleSource = 190;
-    public static final double k_pivotAngleAmp = k_pivotAngleSource;
-    public static final double k_pivotAngleStow = 275;
-
-    // Intake speeds
-    public static final double k_intakeSpeed = 0.7;
-    public static final double k_ejectSpeed = -0.45;
-    public static final double k_feedShooterSpeed = -0.5;
-  }
-
-  // PCM
-  public static final int kPCMId = 0;
-  public static final int kIntakeSolenoidForwardId = 2;
-
-  // DIO
-
-  // Shooter
-  public static final int kShooterLeftMotorId = 12;
-  public static final int kShooterRightMotorId = 13;
-
-  public static final double kShooterP = 0.00005;
-  public static final double kShooterI = 0.0;
-  public static final double kShooterD = 0.0;
-  public static final double kShooterFF = 0.0002;
-
-  public static final double kShooterMinOutput = 0;
-  public static final double kShooterMaxOutput = 1;
-
-  // Climber
-  public static final int kClimberLeftMotorId = 14;
-  public static final int kClimberRightMotorId = 15;
-  public static final double kClimberClimbSpeed = 600.0; // RPM
-  public static final double kClimberReleaseSpeed = -600.0; // RPM
-
-  public static final double kClimberGearRatio = 1.0 / 12.0;
-
-  public static final double kClimberP = 0.001;
-  public static final double kClimberI = 0.0;
-  public static final double kClimberD = 0.0;
-  public static final double kClimberMinOutput = -0.5;
-
-  public static final double kClimberMaxOutput = 0.5;
-
-  // Drivetrain
-  public static class Drive {
-    public static final double kP = 0.0; // 0.00085;
-    public static final double kI = 0.0;
-    public static final double kD = 0.0;
-
-    public static final double kS = 0.1695;// 0.01;
-    public static final double kV = 2.8559;// 2.6;
-    public static final double kA = 0.4864;
-
-    public static final int kFLMotorId = 8;
-    public static final int kBLMotorId = 7;
-    public static final int kFRMotorId = 6;
-    public static final int kBRMotorId = 5;
-  }
-
+  
+  
+// test this
   public static class Field {
     public static final double k_width = Units.feetToMeters(54.0);
     public static final double k_length = Units.feetToMeters(27.0);
 
-    // TODO: Add left and right subwoofer starting poses
     public static final Pose2d redCenterPose2d = new Pose2d(15.19, 5.50, new Rotation2d(Units.degreesToRadians(180.0)));
     public static final Pose2d blueCenterPose2d = new Pose2d(1.27, 5.50, new Rotation2d(0));
   }
@@ -182,3 +198,4 @@ public class Constants {
     public static final int k_totalLength = 300;
   }
 }
+
